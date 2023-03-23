4f78d4d1cc5a094d7edde86f84f691a9f3b718b9
Moses Fawole <mosesfawole2001@gmail.com>
2023-03-23 13:38:07 +0100

    initial header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import { useState } from "react";
-function App() {
-  return <div className="App"></div>;
+import Header from "./components/Header";
+function App(props) {
+  const { selectedFont } = props;
+  console.log(selectedFont);
+  return (
+    <div
+      className={`App font-${selectedFont} md:max-w-[800px] m-auto p-20 px-16`}
+    >
+      <Header />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,71 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Logo from "../assets/logo.svg";
+import Moon from "../assets/moon.svg";
+import ArrowDown from "../assets/icon-arrow-down.svg";
+import ArrowUp from "../assets/icon-arrow-up.svg";
 const Header = () => {
-  return <div>Header</div>;
+  const [selectedFont, setSelectedFont] = useState("Sans-serif");
+  const [modal, setModal] = useState(true);
+
+  return (
+    <div>
+      <div className="header flex">
+        <div className="logo flex-1">
+          <img src={Logo} alt="logo" />
+        </div>
+        <div className="rest flex gap-4 items-center justify-center">
+          <div className="fonts relative">
+            <div className="font-bold flex capitalize gap-4">
+              {selectedFont}
+              <img
+                onClick={() => {
+                  setModal(!modal);
+                }}
+                className={`${
+                  modal ? "rotate-180  " : ""
+                } cursor-pointer transition-all`}
+                src={modal ? ArrowUp : ArrowDown}
+                alt="down-arrow"
+              />
+            </div>
+
+            <div
+              className={`${
+                modal
+                  ? "absolute  flex flex-col justify-center gap-2 font-bold z-10 right-4 top-8 w-48 h-32 p-4 bg-white shadow-2xl rounded-xl "
+                  : "hidden"
+              }  `}
+            >
+              <p
+                onClick={() => setSelectedFont("sans-serif")}
+                className="cursor-pointer font-sans-serif"
+              >
+                Sans-Serif
+              </p>
+              <p
+                onClick={() => setSelectedFont("serif")}
+                className="cursor-pointer font-serif"
+              >
+                Serif
+              </p>
+              <p
+                onClick={() => setSelectedFont("mono")}
+                className="cursor-pointer font-mono"
+              >
+                Mono
+              </p>
+            </div>
+          </div>
+          {/* seperator */}
+          <div className="seperator h-10 border"></div>
+          <div className="theme"></div>
+          <div className="moon">
+            <img src={Moon} alt="moon" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
