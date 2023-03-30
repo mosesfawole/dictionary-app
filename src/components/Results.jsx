94039774359660b7ca563dca3686d39807daced8
Moses Fawole <mosesfawole2001@gmail.com>
2023-03-30 21:38:22 +0100

    initial complete data render lol

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -25,7 +25,7 @@ const Results = () => {
         setError({
           emoji: "😕",
           title: "No Definitions Found",
-          message: `Sorry pal, we couldn't find definitions for the word ${searchTerm} you were looking for.`,
+          message: `Sorry pal, we couldn't find definitions for the word (${searchTerm}) you were looking for.`,
           resolution:
             "You can try the search again at later time or head to the web instead.",
         });
