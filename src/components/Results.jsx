a2423452695d577186f988e16590a34c632b71b0
Moses Fawole <mosesfawole2001@gmail.com>
2023-03-30 21:19:03 +0100

    re-render data with another logic

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,28 +3,27 @@ import Search from "../assets/icon-search.svg";
 import Play from "../assets/icon-play.svg";
 import Axios from "axios";
 const Results = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [error, setError] = useState("");
-  const [isPlay, setIsPlay] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     Axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchTerm}`)
       .then((response) => {
+        setData(response.data);
+        console.log(response.data);
         setError("");
-        console.log(response.data[0]);
-        setData(response.data[0]);
       })
       .catch((error) => {
         setError(error);
       });
   };
 
-  const playAudio = () => {
-    let audio = new Audio(data.phonetics[0].audio);
-    audio.play();
+  const playAudio = (audio) => {
+    let audioElement = new Audio(audio);
+    audioElement.play();
   };
 
   return (
@@ -47,54 +46,75 @@ const Results = () => {
         </form>
         <div className="result">
           {error}
-          {data && (
-            <div className="">
-              <div className="top">
-                <h1 className="text-6xl font-bold"> {data.word}</h1>
-                <p>{data.phonetics[0].text}</p>
-                {data.phonetics[0].audio && (
-                  <button onClick={playAudio}>
-                    <img src={Play} alt="play" />
-                  </button>
-                )}
-              </div>
+          {data.slice(0, 1).map((result, index) => {
+            return (
+              <div className="" key={index}>
+                <div className="header">
+                  <h2>{result.word}</h2>
+                  {result.phonetics.map(
+                    (phonetic, index) =>
+                      phonetic.audio && (
+                        <button key={index}>
+                          <img
+                            className="cursor-pointer"
+                            key={index}
+                            src={Play}
+                            alt="play-icon"
+                            onClick={() => playAudio(phonetic.audio)}
+                          />
+                        </button>
+                      )
+                  )}
 
-              <div className="">
-                <div>
-                  {data.meanings.map((meaning, index) => (
-                    <div className="" key={index}>
-                      <h2>{meaning.partOfSpeech}</h2>
-                      <ul>
-                        {meaning.definitions.map((definition, index) => (
-                          <li key={index}>
-                            <p>{definition.definition}</p>
-                            <p>{definition.example}</p>
+                  <h4>{result.phonetic}</h4>
+                </div>
+                <div className="main">
+                  {result.meanings.slice(0, 2).map((meaning, index) => {
+                    return (
+                      <div className="" key={index}>
+                        <div className="speech">
+                          <h3>{meaning.parOfSpeech}</h3>
+                          <div className="line"></div>
+                        </div>
+                        <div className="definitions">
+                          <h4>Meaning</h4>
+                        </div>
+                        {meaning.definitions
+                          .slice(0, 3)
+                          .map((definition, index) => {
+                            return (
+                              <div className="" key={index}>
+                                <ul>
+                                  <li>{definition.definition}</li>
+                                </ul>
+                                {definition.example && (
+                                  <div className="example">
+                                    <p>"{definition.example}"</p>
+                                  </div>
+                                )}
+                              </div>
+                            );
+                          })}
+                        <div className="synonyms">
+                          {meaning.synonyms && meaning.synonyms.length > 0 && (
+                            <span>
+                              <h4>Synonyms</h4>: {meaning.synonyms.join(", ")}
+                            </span>
+                          )}
 
-                            <div>
-                              {meaning.synonyms &&
-                                meaning.synonyms.length !== 0 && (
-                                  <p>Synonyms: {meaning.synonyms.join(", ")}</p>
-                                )}
-                              {meaning.antonyms &&
-                                meaning.antonyms.length !== 0 && (
-                                  <p>Antonyms: {meaning.antonyms.join(", ")}</p>
-                                )}
-                            </div>
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  ))}
+                          {meaning.antonyms && meaning.antonyms.length > 0 && (
+                            <span>
+                              <h4>Antonyms</h4>: {meaning.antonyms.join(", ")}
+                            </span>
+                          )}
+                        </div>
+                      </div>
+                    );
+                  })}
                 </div>
               </div>
-              <div className="">
-                Source:{" "}
-                <a className="" href={data.sourceUrls[0]}>
-                  {data.sourceUrls[0]}
-                </a>
-              </div>
-            </div>
-          )}
+            );
+          })}
         </div>
       </div>
     </div>
